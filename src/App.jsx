f23cf63471f06a7c7ff03e7f67fa32255da662c0
Megan <830037820@wwcc.edu>
2024-05-27 18:10:55 -0700

    Function Based TaskList working V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
 import './App.css';
 import TaskList from './Tasklist';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        <TaskList />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="app-container">
+      <TaskList />
+    </div>
+  );
 }
 
 export default App;
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,120 +1,104 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import "./App.css";
 import EmptyTasksGraphic from './assets/undraw_to_do_list_re_9nt7.svg';
 import TrashCanIcon from './assets/iconmonstr-trash-can-lined.svg';
 
-  class TaskList extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        tasks: [],
-        taskInput: ''
-      };
-      this.taskInputRef = React.createRef();
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
+  const taskInputRef = useRef();
+
+  const handleInputChange = (e) => {
+    setTaskInput(e.target.value);
+  };
+
+  const addTask = () => {
+    if (taskInput.trim() !== '') {
+      setTasks(prevTasks => [{ text: taskInput, checked: false }, ...prevTasks]);
+      setTaskInput('');
     }
-  
-    handleInputChange = (e) => {
-      this.setState({ taskInput: e.target.value });
-    };
-  
-    addTask = () => {
-      if (this.state.taskInput.trim() !== '') {
-        this.setState(prevState => ({
-          tasks: [{ text: this.state.taskInput, checked: false }, ...prevState.tasks],
-          taskInput: ''
-        }), () => {
-          this.taskInputRef.current.focus(); // Focus the input box after state update
-        });
-      }
-    };
-  
-    deleteTask = (index) => {
-        // Create a copy of the tasks array
-        const tasksCopy = [...this.state.tasks];
-      
-        // Only delete the task if it is checked
-        if (tasksCopy[index].checked) {
-          tasksCopy.splice(index, 1);
-      
-          // Update the state with the new tasks array
-          this.setState({ tasks: tasksCopy });
+  };
+
+  const deleteTask = (taskToDelete) => {
+    setTasks(prevTasks => prevTasks.filter(task => task !== taskToDelete));
+  };
+
+  const toggleCheck = (index) => {
+    setTasks(prevTasks => prevTasks.map((task, i) => {
+      if (i === index) {
+        // If the task is being checked, set a timeout to delete it after 4 seconds
+        if (!task.checked) {
+          const taskToDelete = { ...task, checked: !task.checked };
+          setTimeout(() => {
+            deleteTask(taskToDelete);
+          }, 4000);
+          return taskToDelete;
         }
-      };
-      
-  
-    toggleCheck = (index) => {
-      this.setState(prevState => {
-        const tasks = [...prevState.tasks];
-        tasks[index] = { ...tasks[index], checked: !tasks[index].checked };
-        return { tasks };
-      });
-    };
-  
-    handleKeyDown = (e) => {
-      if (e.key === 'Enter') {
-        this.addTask();
+        return { ...task, checked: !task.checked };
       }
-    };
-  
-    render() {
-      const { tasks, taskInput } = this.state;
-      return (
-        <div className="task-list">
-           <h1 className="app-title">Task List</h1>
-          <div className="task-input-container">
-            <input
-              type="text"
-              className="task-input"
-              value={taskInput}
-              onChange={this.handleInputChange}
-              onKeyDown={this.handleKeyDown}
-              placeholder="Enter a new task"
-              ref={this.taskInputRef}
-            />
-            <button className="create-task-button" onClick={this.addTask}>
-              Create
-            </button>
-          </div>
-          {tasks.length > 0 ? (
-            <ul>
-              {tasks.map((task, index) => {
-                // If the task is checked, set a timeout to delete it after 4 seconds
-                if (task.checked) {
-                  setTimeout(() => {
-                    this.deleteTask(index);
-                  }, 4000);
-                }
-                return (
-                  <li key={index}>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                      <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <input
-                          type="checkbox"
-                          checked={task.checked}
-                          onChange={() => this.toggleCheck(index)}
-                        />
-                        <span style={{ flexGrow: 1, textDecoration: task.checked ? 'line-through' : 'none' }}>{task.text}</span>
-                      </div>
-                      <div>
-                        <button className="delete-task-button" onClick={() => this.deleteTask(index)}>
-                          <img src={TrashCanIcon} alt="Delete task" style={{width: "16px", height: "16px"}} />
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          ) : (
-            <div className="empty-tasks">
-              <img src={EmptyTasksGraphic} alt="No tasks" style={{width: "400px", height: "230.43px"}} />
-              <p>No tasks yet!</p>
-            </div>
-          )}
+      return task;
+    }));
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      addTask();
+    }
+  };
+
+  useEffect(() => {
+    if (taskInput === '') {
+      taskInputRef.current.focus();
+    }
+  }, [taskInput]);
+
+  return (
+    <div className="task-list">
+      <h1 className="app-title">Task List</h1>
+      <div className="task-input-container">
+        <input
+          type="text"
+          className="task-input"
+          value={taskInput}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          placeholder="Enter a new task"
+          ref={taskInputRef}
+        />
+        <button className="create-task-button" onClick={addTask}>
+          Create
+        </button>
+      </div>
+      {tasks.length > 0 ? (
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <input
+                    type="checkbox"
+                    checked={task.checked}
+                    onChange={() => toggleCheck(index)}
+                  />
+                  <span style={{ flexGrow: 1, textDecoration: task.checked ? 'line-through' : 'none' }}>{task.text}</span>
+                </div>
+                <div>
+                  <button className="delete-task-button" onClick={() => deleteTask(index)}>
+                    <img src={TrashCanIcon} alt="Delete task" style={{width: "16px", height: "16px"}} />
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty-tasks">
+          <img src={EmptyTasksGraphic} alt="No tasks" style={{width: "400px", height: "230.43px"}} />
+          <p>No tasks yet!</p>
         </div>
-      );
-    }  
-  }
-  
-  export default TaskList;
-  
+      )}
+    </div>
+  );
+}
+
+export default TaskList;
